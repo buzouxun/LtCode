e57d60b22119a8c563ce463606d9059f7e6b393b
Junyu <zhujunyu.njcn@gmail.com>
2015-08-03 17:51:48 -0400

    8-3-2015

--- src/medium/ID_052_LongestSubstringWithoutRepeatingCharacters.java
+++ src/medium/ID_052_LongestSubstringWithoutRepeatingCharacters.java
@@ -1,18 +1,41 @@
 package medium;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ID_052_LongestSubstringWithoutRepeatingCharacters {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		ID_052_LongestSubstringWithoutRepeatingCharacters problem = new ID_052_LongestSubstringWithoutRepeatingCharacters();
+
+		System.out.println("'abca' = " + problem.new Solution().lengthOfLongestSubstring("abcaaabcc"));
+		//		System.out.println("'abcccxyz' = " + problem.new Solution().lengthOfLongestSubstring("abcccxyz"));
+		//		System.out.println("'abccbcxyz' = " + problem.new Solution().lengthOfLongestSubstring("abccbcxyz"));
 
 	}
-
 	class Solution {
 		public int lengthOfLongestSubstring(String s) {
-			int res = 0;
-			
-			return 0;
+			int maxLength = 0;
+			if(s.length() == 0 || s.length() == 1) {
+				maxLength = s.length();
+			}
+			else {
+				HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+				int leftPointer = -1;
+				for(int rightPointer = 0; rightPointer < s.length(); rightPointer++) {
+					char c = s.charAt(rightPointer);
+					if(map.containsKey(c)) {
+						leftPointer = Math.max(leftPointer, map.get(c));
+						System.out.println("char = " + c + ", leftPointer = " + leftPointer);
+					}
+					else {
+						// don't move left pointer
+					}
+					map.put(c, rightPointer);
+					maxLength = Math.max(maxLength, rightPointer - leftPointer);
+				}				
+			}
+			return maxLength;
 		}
 	}
-
 }

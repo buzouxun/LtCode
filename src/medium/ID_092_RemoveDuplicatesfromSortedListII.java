d728bc5f18d96846877c018e834f20f5df32baf9
Junyu <zhujunyu.njcn@gmail.com>
2015-08-18 00:53:14 -0400

    8-18-2015

--- src/medium/ID_092_RemoveDuplicatesfromSortedListII.java
+++ src/medium/ID_092_RemoveDuplicatesfromSortedListII.java
@@ -10,7 +10,33 @@ public class ID_092_RemoveDuplicatesfromSortedListII {
 	class Solution {
 		public ListNode deleteDuplicates(ListNode head) {
 			ListNode res = new ListNode(-1);
-			
+			ListNode newHead = res;
+			ListNode first = null;
+			ListNode second = null;
+			ListNode tmp = head;
+			while(tmp != null) {
+				if(first == null) {
+					first = tmp;
+				}
+				else {
+					if(tmp.val == first.val) {
+						second = tmp;
+					}
+					else {
+						if(second == null) {
+							newHead.next = first;
+							first.next = null;
+							newHead = newHead.next;
+						}
+						first = tmp;
+						second = null;
+					}
+				}
+				tmp = tmp.next;
+			}
+			if(first != null && second == null) {
+				newHead.next = first;
+			}
 			return res.next;
 		}
 	}

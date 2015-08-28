83fbf166de1681fac9442d8ad6bb6c047120c258
Junyu <zhujunyu.njcn@gmail.com>
2015-08-28 00:29:17 -0400

    8-28-2015

--- src/medium/ID_104_ConstructBinaryTreefromPreorderandInorderTraversal.java
+++ src/medium/ID_104_ConstructBinaryTreefromPreorderandInorderTraversal.java
@@ -5,8 +5,13 @@ import java.util.*;
 public class ID_104_ConstructBinaryTreefromPreorderandInorderTraversal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		ID_104_ConstructBinaryTreefromPreorderandInorderTraversal problem = new ID_104_ConstructBinaryTreefromPreorderandInorderTraversal();
 
+		//		System.out.println(problem.new Solution().buildTree(new int[]{1,2,3}, new int[]{3,2,1}).left.left.val);
+		//		System.out.println(problem.new Solution().buildTree(new int[]{1,2,3}, new int[]{1,2,3}).val);
+		//		System.out.println(problem.new Solution().buildTree(new int[]{4,1,2,3}, new int[]{1,2,3,4}).left.right.right.val);
+		//				System.out.println(problem.new Solution().buildTree(new int[]{2,1,3,4}, new int[]{1,2,3,4}).right.right.val);
+		System.out.println(problem.new Solution().buildTree(new int[]{2,1,3,4}, new int[]{1,2,3,4}).val);
 	}
 
 	class Solution {
@@ -20,12 +25,96 @@ public class ID_104_ConstructBinaryTreefromPreorderandInorderTraversal {
 				for(pre = 1; pre < preorder.length; pre++) {
 					int next = preorder[pre];
 					int curr = preorder[0];
-					// left or right
-					int pos = findLeftOrRight(inorder, curr, next);
-					// make new node
-					
-					// if fail, try again
-					
+
+					//					if(next == 3) { 
+					//						System.out.println("HAHAHA"); 
+					//					}
+
+					boolean makeNew = false;
+					TreeNode[] subTree = new TreeNode[1];
+					TreeNode subRoot = root;
+					while(makeNew == false) {
+						// left or right
+						int pos = findLeftOrRight(inorder, curr, next);
+						// make new node
+						int newNode = makeNewNode(subRoot, pos, curr, next, subTree);
+
+						//						System.out.println("newNOde = " + newNode);
+
+						// if fail, try again
+						if(newNode == next) {
+							makeNew = true;
+						}
+						else {
+							// fail
+							curr = newNode;
+							subRoot = subTree[0];
+						}
+					}
+				}
+				return root;
+			}
+		}
+
+		private int makeNewNode(TreeNode root, int pos, int curr, int next, TreeNode[] subTree) {
+			if(root.val == curr) {
+				if(pos == -1) {
+					// left
+					if(root.left == null) {
+						root.left = new TreeNode(next);
+						return next;
+					}
+					else {
+						subTree[0] = root.left;
+						return root.left.val;
+					}
+				}
+				else {
+					// right
+					if(root.right == null) {
+						root.right = new TreeNode(next);
+						return next;
+					}
+					else {
+						subTree[0] = root.right;
+						return root.right.val;
+					}
+				}
+			}
+			else {
+				int found = curr;
+				if(root.left == null && root.right == null) {
+					found = curr;
+				}
+				else if(root.left != null && root.right == null) {
+					found = makeNewNode(root.left, pos, curr, next, subTree);
+				}
+				else if(root.left == null && root.right != null) {
+					found = makeNewNode(root.right, pos, curr, next, subTree);
+				}
+				else {
+					found = makeNewNode(root.left, pos, curr, next, subTree);
+					if(found != next) {
+						found = makeNewNode(root.right, pos, curr, next, subTree);
+					}
+				}
+				if(found != curr) {
+					return found;
+				}
+				else {
+					//					System.out.println("Why found = " + found);
+					//					System.out.println("Why curr = " + curr);
+					//					System.out.println("Why next = " + next);
+
+					if(root.left != null && pos == -1) {
+						return root.left.val;
+					}
+					else if(root.right != null && pos == 1) {
+						return root.right.val;
+					}
+					else {
+						return curr;
+					}
 				}
 			}
 		}
@@ -50,6 +139,7 @@ public class ID_104_ConstructBinaryTreefromPreorderandInorderTraversal {
 				pos = -1;
 			}
 			else {
+				// right
 				pos = 1;
 			}
 			return pos;

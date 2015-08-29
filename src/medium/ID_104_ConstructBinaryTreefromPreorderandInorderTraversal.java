aa848ebea99adb3623c937c63b76239a75396e2b
Junyu <zhujunyu.njcn@gmail.com>
2015-08-28 20:10:37 -0400

    8-28-2015

--- src/medium/ID_104_ConstructBinaryTreefromPreorderandInorderTraversal.java
+++ src/medium/ID_104_ConstructBinaryTreefromPreorderandInorderTraversal.java
@@ -16,141 +16,134 @@ public class ID_104_ConstructBinaryTreefromPreorderandInorderTraversal {
 
 	class Solution {
 		public TreeNode buildTree(int[] preorder, int[] inorder) {
-			if(preorder.length == 0 || inorder.length == 0) {
-				return null;
-			}
-			else {
-				TreeNode root = new TreeNode(preorder[0]);
-				int pre = 1;
-				for(pre = 1; pre < preorder.length; pre++) {
-					int next = preorder[pre];
-					int curr = preorder[0];
+			TreeNode root = new TreeNode(preorder[0]);
+			
+			return root;
 
-					//					if(next == 3) { 
-					//						System.out.println("HAHAHA"); 
-					//					}
-
-					boolean makeNew = false;
-					TreeNode[] subTree = new TreeNode[1];
-					TreeNode subRoot = root;
-					while(makeNew == false) {
-						// left or right
-						int pos = findLeftOrRight(inorder, curr, next);
-						// make new node
-						int newNode = makeNewNode(subRoot, pos, curr, next, subTree);
-
-						//						System.out.println("newNOde = " + newNode);
-
-						// if fail, try again
-						if(newNode == next) {
-							makeNew = true;
-						}
-						else {
-							// fail
-							curr = newNode;
-							subRoot = subTree[0];
-						}
-					}
-				}
-				return root;
-			}
+			// Myself
+			//			if(preorder.length == 0 || inorder.length == 0) {
+			//				return null;
+			//			}
+			//			else {
+			//				TreeNode root = new TreeNode(preorder[0]);
+			//				int pre = 1;
+			//				for(pre = 1; pre < preorder.length; pre++) {
+			//					int next = preorder[pre];
+			//					int curr = preorder[0];
+			//					boolean makeNew = false;
+			//					TreeNode[] subTree = new TreeNode[1];
+			//					TreeNode subRoot = root;
+			//					while(makeNew == false) {
+			//						// left or right
+			//						int pos = findLeftOrRight(inorder, curr, next);
+			//						// make new node
+			//						int newNode = makeNewNode(subRoot, pos, curr, next, subTree);
+			//						// if fail, try again
+			//						if(newNode == next) {
+			//							makeNew = true;
+			//						}
+			//						else {
+			//							// fail
+			//							curr = newNode;
+			//							subRoot = subTree[0];
+			//						}
+			//					}
+			//				}
+			//				return root;
 		}
+	}
 
-		private int makeNewNode(TreeNode root, int pos, int curr, int next, TreeNode[] subTree) {
-			if(root.val == curr) {
-				if(pos == -1) {
-					// left
-					if(root.left == null) {
-						root.left = new TreeNode(next);
-						return next;
-					}
-					else {
-						subTree[0] = root.left;
-						return root.left.val;
-					}
-				}
-				else {
-					// right
-					if(root.right == null) {
-						root.right = new TreeNode(next);
-						return next;
-					}
-					else {
-						subTree[0] = root.right;
-						return root.right.val;
-					}
-				}
-			}
-			else {
-				int found = curr;
-				if(root.left == null && root.right == null) {
-					found = curr;
-				}
-				else if(root.left != null && root.right == null) {
-					found = makeNewNode(root.left, pos, curr, next, subTree);
-				}
-				else if(root.left == null && root.right != null) {
-					found = makeNewNode(root.right, pos, curr, next, subTree);
-				}
-				else {
-					found = makeNewNode(root.left, pos, curr, next, subTree);
-					if(found != next) {
-						found = makeNewNode(root.right, pos, curr, next, subTree);
-					}
-				}
-				if(found != curr) {
-					return found;
-				}
-				else {
-					//					System.out.println("Why found = " + found);
-					//					System.out.println("Why curr = " + curr);
-					//					System.out.println("Why next = " + next);
-
-					if(root.left != null && pos == -1) {
-						return root.left.val;
-					}
-					else if(root.right != null && pos == 1) {
-						return root.right.val;
-					}
-					else {
-						return curr;
-					}
-				}
-			}
-		}
-
-		private int findLeftOrRight(int[] array, int curr, int next) {
-			int pos = 0;
-			int curr_pos = -1;
-			int next_pos = -1;
-			for(int i = 0; i < array.length; i++) {
-				if(array[i] == curr) {
-					curr_pos = i;
-				}
-				if(array[i] == next) {
-					next_pos = i;
-				}
-				if(curr_pos != -1 && next_pos != -1) {
-					break;
-				}	
-			}
-			if(next_pos < curr_pos) {
+	private int makeNewNode(TreeNode root, int pos, int curr, int next, TreeNode[] subTree) {
+		if(root.val == curr) {
+			if(pos == -1) {
 				// left
-				pos = -1;
+				if(root.left == null) {
+					root.left = new TreeNode(next);
+					return next;
+				}
+				else {
+					subTree[0] = root.left;
+					return root.left.val;
+				}
 			}
 			else {
 				// right
-				pos = 1;
+				if(root.right == null) {
+					root.right = new TreeNode(next);
+					return next;
+				}
+				else {
+					subTree[0] = root.right;
+					return root.right.val;
+				}
 			}
-			return pos;
+		}
+		else {
+			int found = curr;
+			if(root.left == null && root.right == null) {
+				found = curr;
+			}
+			else if(root.left != null && root.right == null) {
+				found = makeNewNode(root.left, pos, curr, next, subTree);
+			}
+			else if(root.left == null && root.right != null) {
+				found = makeNewNode(root.right, pos, curr, next, subTree);
+			}
+			else {
+				found = makeNewNode(root.left, pos, curr, next, subTree);
+				if(found != next) {
+					found = makeNewNode(root.right, pos, curr, next, subTree);
+				}
+			}
+			if(found != curr) {
+				return found;
+			}
+			else {
+				if(root.left != null && pos == -1) {
+					return root.left.val;
+				}
+				else if(root.right != null && pos == 1) {
+					return root.right.val;
+				}
+				else {
+					return curr;
+				}
+			}
 		}
 	}
 
-	class TreeNode {
-		int val;
-		TreeNode left;
-		TreeNode right;
-		TreeNode(int x) { val = x; }
+	private int findLeftOrRight(int[] array, int curr, int next) {
+		int pos = 0;
+		int curr_pos = -1;
+		int next_pos = -1;
+		for(int i = 0; i < array.length; i++) {
+			if(array[i] == curr) {
+				curr_pos = i;
+			}
+			if(array[i] == next) {
+				next_pos = i;
+			}
+			if(curr_pos != -1 && next_pos != -1) {
+				break;
+			}	
+		}
+		if(next_pos < curr_pos) {
+			// left
+			pos = -1;
+		}
+		else {
+			// right
+			pos = 1;
+		}
+		return pos;
 	}
+}
+
+class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+	TreeNode(int x) { val = x; }
+}
 
 }

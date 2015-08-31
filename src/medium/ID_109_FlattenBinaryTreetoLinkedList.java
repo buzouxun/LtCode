ad9dd03388dfb3236ccf788fb4bcc73285d5b934
Junyu <zhujunyu.njcn@gmail.com>
2015-08-31 00:26:08 -0400

    8-31-2015

--- src/medium/ID_109_FlattenBinaryTreetoLinkedList.java
+++ src/medium/ID_109_FlattenBinaryTreetoLinkedList.java
@@ -7,30 +7,62 @@ public class ID_109_FlattenBinaryTreetoLinkedList {
 
 		TreeNode root = problem.new TreeNode(1);
 		root.left = problem.new TreeNode(2);
-		System.out.println(root.left.val);
+		root.left.left = problem.new TreeNode(3);
+		root.left.left = problem.new TreeNode(4);
+		root.right = problem.new TreeNode(5);
+		root.right.right = problem.new TreeNode(6);
+
+		long num = 7200;
+
+		TreeNode tmp = root.left.left;
+		for(long i = 0; i < num; i++) {
+			tmp.left = problem.new TreeNode(9);
+			tmp = tmp.left;
+		}
+		tmp = root.right.right;
+		for(long i = 0; i < num; i++) {
+			tmp.right = problem.new TreeNode(8);
+			tmp = tmp.right;
+		}
+
+		//		System.out.println(root.left.val);
+		long start = System.currentTimeMillis();
 		problem.new Solution().flatten(root);
-		System.out.println(root.right.val);
+		System.out.println(System.currentTimeMillis() - start);
+		//		System.out.println(root.right.val);
 	}
 
 	class Solution {
-		
+
 		TreeNode root2ForRight;
 
 		//   1       1
 		//  2 3        2
 		//               3
 		public void flatten(TreeNode root) {
-			if(root == null) {
-
+			while (root != null) {
+				if (root.left != null) { // check left child
+					TreeNode n = root.left;
+					while (n.right != null) n = n.right; // rightmost child of left
+					n.right = root.right; // insert right subtree to its right
+					root.right = root.left; // set left subtree as right subtree
+					root.left = null; // set left to null
+				}
+				root = root.right; // move to right child
 			}
-			else {
-				//				TreeNode newRoot = root;
-				TreeNode[] rootForRight = new TreeNode[]{new TreeNode(-1)};
-				root2ForRight = root;
-				root = helper2(root);
-			}			
+
+			// /* myself */
+			//			if(root == null) {
+			//
+			//			}
+			//			else {
+			//				//				TreeNode newRoot = root;
+			//				TreeNode[] rootForRight = new TreeNode[]{new TreeNode(-1)};
+			//				root2ForRight = root;
+			//				root = helper2(root);
+			//			}			
 		}
-		
+
 		private TreeNode helper2(TreeNode root) {
 			if(root == null) {
 				// do nothing

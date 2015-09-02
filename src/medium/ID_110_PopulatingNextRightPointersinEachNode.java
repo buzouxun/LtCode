d382c0e244b740ec27eeafaf4b5794ba71ed230c
Junyu <zhujunyu.njcn@gmail.com>
2015-09-01 21:54:23 -0400

    9-1-2015

---
+++ src/medium/ID_110_PopulatingNextRightPointersinEachNode.java
@@ -0,0 +1,16 @@
+package medium;
+
+public class ID_110_PopulatingNextRightPointersinEachNode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	class Solution {
+	    public void connect(TreeLinkNode root) {
+	        
+	    }
+	}
+
+}
